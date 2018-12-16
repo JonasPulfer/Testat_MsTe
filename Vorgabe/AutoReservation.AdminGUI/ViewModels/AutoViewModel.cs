85c3f49e23ee77e5b7cbfd4fe0d570746cb14aaa
JonasPulfer <38106520+JonasPulfer@users.noreply.github.com>
2018-12-16 16:41:00 +0100

    Save / Delete funktioniert, Add mol was probiert

--- Vorgabe/AutoReservation.AdminGUI/ViewModels/AutoViewModel.cs
+++ Vorgabe/AutoReservation.AdminGUI/ViewModels/AutoViewModel.cs
@@ -1,46 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows;
+using AutoReservation.AdminGUI.ViewModels.Commands;
 using AutoReservation.Common.DataTransferObjects;
-using AutoReservation.Dal.Entities;
 
 namespace AutoReservation.AdminGUI.ViewModels
 {
     class AutoViewModel : BindableBase
     {
+        private int _index;
+
         public AutoViewModel()
         {
             Autos = Target.GetAutoList();
+            DeleteButtonClick = new RelayCommand(DeleteAuto, () => CanDeleteAuto);
+            SaveButtonClick = new RelayCommand(Save, () => CanSave);
+            AddButtonClick = new RelayCommand(Add);
+
+            Index = -1;
         }
 
-        private List<AutoDto> _autos;
-
-        public List<AutoDto> Autos
+        public int Index
         {
-            get
-            {
-                return _autos;
-            }
+            get { return _index; }
             set
             {
-                _autos = new List<AutoDto>(value);
+                _index = value;
+                SelectedIndexChanged();
             }
         }
+
+        public List<AutoDto> Autos { get; set; }
+
+        public RelayCommand DeleteButtonClick { get; set; }
+        public RelayCommand SaveButtonClick { get; set; }
+        public RelayCommand AddButtonClick { get; set; }
 
         public AutoDto CurrentAuto { get; set; }
 
-        public void SelectedIndexChanged(object sender, System.EventArgs e)
+        public void SelectedIndexChanged()
         {
-            ListBox ourList = (ListBox)sender;
-            int index = ourList.SelectedIndex;
-            if (index >= 0)
+            if (Index >= 0)
             {
-                CurrentAuto = Autos[index];
+                CurrentAuto = Autos[Index];
+                DeleteButtonClick.RaiseCanExecuteChanged();
                 OnPropertyChanged(nameof(CurrentAuto));
             }
+        }
+
+        public void Add()
+        {
+            Index = -1;
+            CurrentAuto = new AutoDto();
+
+            SaveButtonClick.RaiseCanExecuteChanged();
+        }
+
+        public bool CheckInput()
+        {
+            if (CurrentAuto.Marke == "" || CurrentAuto.AutoKlasse.ToString() == ""  || CurrentAuto.Basistarif.ToString() == "")
+            {
+                string messageBoxText = "invalid names! Please check the input.";
+                string caption = "Invalid Input";
+
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Error;
+
+                MessageBox.Show(messageBoxText, caption, button, icon);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Save()
+        {
+            if (!CheckInput())
+            {
+                return;
+            }
+
+            foreach (AutoDto collectionDto in Autos)
+            {
+                if (CurrentAuto.Id == collectionDto.Id)
+                {
+                    Target.UpdateAuto(CurrentAuto);
+                    Refresh();
+                    return;
+                }
+            }
+
+            AutoDto autoToBeInserted = new AutoDto
+            {
+                AutoKlasse = CurrentAuto.AutoKlasse,
+                Basistarif = CurrentAuto.Basistarif,
+                Marke = CurrentAuto.Marke,
+                Tagestarif = CurrentAuto.Tagestarif
+            };
+
+            Target.InsertAuto(autoToBeInserted);
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            Autos = Target.GetAutoList();
+            CurrentAuto = null;
+
+            OnPropertyChanged(nameof(Autos));
+            OnPropertyChanged(nameof(CurrentAuto));
+        }
+
+        public bool CanDeleteAuto => CurrentAuto != null;
+        public bool CanSave => CurrentAuto != null;
+
+        public void DeleteAuto()
+        {
+            Target.DeleteAuto(CurrentAuto);
+
+            Refresh();
         }
     }
 }
--- Vorgabe/AutoReservation.AdminGUI/ViewModels/BindableBase.cs
+++ Vorgabe/AutoReservation.AdminGUI/ViewModels/BindableBase.cs
@@ -26,17 +26,7 @@ namespace AutoReservation.AdminGUI.ViewModels
                 return _target;
             }
         }
-
-        protected bool SetProperty<T>(ref T field, T value, string name = null)
-        {
-            if (Equals(field, value))
-                return false;
-
-            field = value;
-            OnPropertyChanged(name);
-            return true;
-        }
-
+        
         protected void OnPropertyChanged(string name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
