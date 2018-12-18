62b8a8be9aee454ce6988fd78e2803b877cc28ce
Fabio <Fabio@hsr.xh>
2018-12-18 14:22:14 +0100

    ListView / ButtosStyle / GenericRelayCommand / Sort Logik

--- Vorgabe/AutoReservation.AdminGUI/ViewModels/AutoViewModel.cs
+++ Vorgabe/AutoReservation.AdminGUI/ViewModels/AutoViewModel.cs
@@ -1,5 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 using AutoReservation.AdminGUI.ViewModels.Commands;
 using AutoReservation.Common.DataTransferObjects;
 
@@ -8,15 +15,87 @@ namespace AutoReservation.AdminGUI.ViewModels
     class AutoViewModel : BindableBase
     {
         private int _index;
+        private int _counter;
 
         public AutoViewModel()
         {
             Autos = Target.GetAutoList();
+
             DeleteButtonClick = new RelayCommand(DeleteAuto, () => CanDeleteAuto);
             SaveButtonClick = new RelayCommand(Save, () => CanSave);
             AddButtonClick = new RelayCommand(Add);
+            SortCommand = new RelayCommand<object>(Sort);
 
             Index = -1;
+        }
+
+
+        public RelayCommand<object> SortCommand { get; set; }
+
+        public void Sort(object parameter)
+        {
+            
+            string column = parameter as string;
+            List<AutoDto> AutosSorted;
+            switch (column)
+            {
+                case "Marke":
+                    if(_counter == 1)
+                    {
+                        AutosSorted = Autos
+                           .OrderByDescending(s => s.Marke)
+                           .ToList();
+                        Autos = AutosSorted;
+                        _counter = 0;
+                    } else
+                    {
+                        AutosSorted = Autos
+                           .OrderBy(s => s.Marke)
+                           .ToList();
+                        Autos = AutosSorted;
+                        _counter++;
+                    }
+                    break;
+                case "Autoklasse":
+                    if (_counter == 1)
+                    {
+                        AutosSorted = Autos
+                           .OrderByDescending(s => s.AutoKlasse)
+                           .ToList();
+                        Autos = AutosSorted;
+                        _counter = 0;
+                    }
+                    else
+                    {
+                        AutosSorted = Autos
+                           .OrderBy(s => s.AutoKlasse)
+                           .ToList();
+                        Autos = AutosSorted;
+                        _counter++;
+                    }
+                    break;
+                case "Tarif":
+                    if (_counter == 1)
+                    {
+                        AutosSorted = Autos
+                           .OrderByDescending(s => s.Tagestarif)
+                           .ToList();
+                        Autos = AutosSorted;
+                        _counter = 0;
+                    }
+                    else
+                    {
+                        AutosSorted = Autos
+                           .OrderBy(s => s.Tagestarif)
+                           .ToList();
+                        Autos = AutosSorted;
+                        _counter++;
+                    }
+                    break;
+
+            }
+
+            OnPropertyChanged(nameof(Autos));
         }
 
         public int Index
@@ -34,7 +113,6 @@ namespace AutoReservation.AdminGUI.ViewModels
         public RelayCommand DeleteButtonClick { get; set; }
         public RelayCommand SaveButtonClick { get; set; }
         public RelayCommand AddButtonClick { get; set; }
-
         public AutoDto CurrentAuto { get; set; }
 
         public void SelectedIndexChanged()
@@ -50,10 +128,16 @@ namespace AutoReservation.AdminGUI.ViewModels
         public void Add()
         {
             Index = -1;
+
             CurrentAuto = new AutoDto();
+
+
+            OnPropertyChanged(nameof(CurrentAuto));
+
 
             SaveButtonClick.RaiseCanExecuteChanged();
         }
+
 
         public bool CheckInput()
         {
--- Vorgabe/AutoReservation.AdminGUI/ViewModels/Commands/RelayCommand.cs
+++ Vorgabe/AutoReservation.AdminGUI/ViewModels/Commands/RelayCommand.cs
@@ -20,5 +20,35 @@ namespace AutoReservation.AdminGUI.ViewModels.Commands
         {
             CanExecuteChanged?.Invoke(this, new EventArgs());
         }
+
+
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            if (execute == null) throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke((T)parameter) ?? true;
+        public void Execute(object parameter) => _execute((T)parameter);
+
+        // event an CommandManager delegieren (Benachrichtigung
+        // erfolgt so immer dann, wenn WPF denkt, dass sich etwas
+        // am Ausführungsstatus geändert hat, z.B. bei Key- oder
+        // Mouse Button-Klick)
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
     }
 }
